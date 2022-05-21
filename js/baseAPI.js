7573cf97378e251c4dd346da74732e3bb26f2f0b
uarsix <15257124299@163.com>
2022-05-21 10:16:15 +0800

    退出功能

--- js/baseAPI.js
+++ js/baseAPI.js
@@ -4,7 +4,27 @@
 $.ajaxPrefilter((option) => {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
   option.url = `http://www.liulongbin.top:3007` + option.url;
-  if(option.url.includes('/my/') ){
-    option.headers={Authorization:localStorage.getItem('token')}
+  if (option.url.includes('/my/')) {
+    option.headers = { Authorization: localStorage.getItem('token') }
   }
+  option.complete = (res) => {
+    // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+    if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+      //  强制清空 token
+      localStorage.removeItem("token");
+      // 强制跳转到登录页面
+      location.href = "/login.html"
+    }
+  }
+
+  // option.complete = (res) => {
+  //   // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+  //   if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+  //     console.log(1);
+  //     //  强制清空 token
+  //     localStorage.removeItem("token");
+  //     // 强制跳转到登录页面
+  //     location.href = "/login.html"
+  //   }
+  // }
 });
--- js/index.js
+++ js/index.js
@@ -1,26 +1,40 @@
-$(function() {
-getUserInfo()
+$(function () {
+    getUserInfo()
+    //退出登陆
+    $("#btnLogout").click(() => {
+        layui.layer.confirm(
+            "确定退出登录？",
+            { icon: 3, title: "" },
+            function (index) {
+                // 清空本地存储里面的 token
+                localStorage.removeItem("token");
+                // 重新跳转到登录页面
+                location.href = "/login.html";
+            }
+        );
+    });
 })
-const layer=layui.layer
-const getUserInfo=()=>{
+// 获取layui
+const layer = layui.layer
+const getUserInfo = () => {
     $.ajax({
         type: "GET",
-        url:'/my/userinfo',
-        
-        success: res=>{
+        url: '/my/userinfo',
+
+        success: res => {
             console.log(res);
-            if(res.status!==0) {layer.msg(res.message)} 
-            else{
+            if (res.status !== 0) {return layer.msg(res.message) }
+            else {
                 layer.msg('请求成功')
             }
             renderAvatar(res.data)
-        }
+        }   
     })
 }
-const renderAvatar=(user)=>{
+const renderAvatar = (user) => {
     //获取名字
-    const name=user.nickname||user.username
-    console.log(user.nickname)
+    let name = user.nickname || user.username
+
     //欢迎文本
     $('#welcome').html(`欢迎${name}`)
     if (user.user_pic !== null) {
@@ -31,6 +45,7 @@ const renderAvatar=(user)=>{
         // 渲染文本头像
         $(".layui-nav-img").hide();
         let firstName = name[0].toUpperCase();
-        $(".text-avatar").html(firstName);
+        $(".text-avatar").html(firstName).show();
     }
 }
+
