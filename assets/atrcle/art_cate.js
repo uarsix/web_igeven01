8f09a91656019598c21b9ef816fc49ab5136e122
uarsix <15257124299@163.com>
2022-05-21 19:58:44 +0800

    更新了文章管理

---
+++ assets/atrcle/art_cate.js
@@ -0,0 +1,43 @@
+$(function() {
+    // 获取 表格数据
+    const initArtCateList = () => {
+        $.ajax({
+            type: "GET",
+            url: "/my/article/cates",
+            success: (res) => {
+                // 调用 template
+                const htmlStr = template("tpl-table", res);
+                $("tbody").empty().html(htmlStr);
+            },
+        });
+    };
+
+    initArtCateList();
+
+    const layer = layui.layer;
+indexAdd=null
+$("#btnAddCate").click(() => {
+    indexAdd=layer.open({
+    type: 1,
+    area: ['500px', '250px'],
+    title: '添加文章分类',
+    content: $('#dialog-add').html()
+})
+});
+
+// 通过代理监听 submit 事件
+$("body").on("submit", "#form-add", function (e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "/my/article/addcates",
+        data: $(this).serialize(),
+        success: (res) => {
+            if (res.status !== 0) return layer.msg("新增分类失败！");
+            initArtCateList();
+            layer.msg("新增分类成功！");
+            layer.close(indexAdd);
+        },
+    });
+});
+})
--- assets/user/userPwd.js
+++ assets/user/userPwd.js
@@ -1,28 +1,31 @@
-$(function () {
-  const form = layui.form
+$(function() {
+  const form = layui.form;
 
-  form.verify({
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+form.verify({
+    pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
     samePwd: (val) => {
-      if (val === $('[name=oldPwd]').val()) return '新旧密码不能相同！'
+        if (val === $("[name=oldPwd]").val()) return "新旧密码不能相同！";
     },
     rePwd: (val) => {
-      if (val !== $('[name=newPwd]').val()) return '两次密码不一致！'
-    }
-  })
-  // 发送请求，重置密码
-  $('.layui-form').on('submit', (e) => {
-    e.preventDefault()
+        if (val !== $("[name=newPwd]").val()) return "两次密码不一致！";
+    },
+});  
+//充值密码
+$(".layui-form").on("submit", function (e) {
+    e.preventDefault();
+    console.log(1);
     $.ajax({
-      type: 'POST',
-      url: '/my/updatepwd',
-      data: $('.layui-form').serialize(),
-      success: (res) => {
-        if (res.status !== 0) return layer.msg('更新密码失败！')
-        layer.msg('更新密码成功！')
-        window.parent.location.href = '/login.html'
-        localStorage.removeItem('token')
-      }
-    })
-  })
+        type: "POST",
+        url: "/my/updatepwd",
+        data:$(this).serialize(),
+        success: (res) => {
+            console.log(res);
+            if (res.status !== 0) return layer.msg("更新密码失败！");
+            layer.msg("更新密码成功！");
+            // 重置表单
+            localStorage.removeItem('token')
+            window.parent.location.href='/login.html'
+        },
+    });
+});
 })
